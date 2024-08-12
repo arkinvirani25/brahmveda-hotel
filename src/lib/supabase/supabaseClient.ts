7679a3ab256dbbb87d7e5a38522a3686bc1a50b4
arkinvirani25 <arkin.virani@brahmveda.com>
2024-08-12 14:08:49 +0530

    resolved build issue

--- src/lib/supabase/supabaseClient.ts
+++ src/lib/supabase/supabaseClient.ts
@@ -1,14 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-export function createClient() {
-  const supabaseStorageUrl = process.env.NEXT_PUBLIC_SUPABASE_STORAGE_URL!;
-
-  return createBrowserClient(
+export const createClient = () =>
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,{global: {
-      headers: {
-        'X-Client-Info': `storage-url=${supabaseStorageUrl}`
-      }
-    }}
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
-}
